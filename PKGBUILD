5340cba6618e280ec6b906f33f48a5b8263188ff
Thomas S Hatch <thatch45@gmail.com>
2010-11-04 23:21:18 -0600

    Working towards viable lvm support, bumped release value to alpha - 0.7.6

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Contributor: Thomas S Hatch <thatch45@gmail.com>
 
 pkgname=varch
-pkgver=0.7.1
+pkgver=0.7.6
 pkgrel=1
 pkgdesc="ArchLinux virtual machine builder"
 arch=(any)
--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@
 from distutils.core import setup
 
 setup(name='virtualarch',
-      version='0.7.1',
+      version='0.7.6',
       description='Virtual machine generator for ArchLinux',
       author='Thomas S Hatch',
       author_email='thatch45@gmail.com',
       url='http://code.google.com/p/enterprise-archlinux/',
       packages=['varch'],
       scripts=['scripts/varch'],
-      data_files=[('/etc/varch', ['conf/base.aif'])],
+      data_files=[('/etc/varch',
+                    ['conf/base.aif',
+                     'conf/base-lvm.aif'])],
      )
 
